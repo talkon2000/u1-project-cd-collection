4d37cd191f88108a916780fa3fedeab42c0d043d
Josh Taylor <107130895+talkon2000@users.noreply.github.com>
2022-08-09 13:33:46 -0500

    Assignment2 tests (#4)
    * Update debugging section of readme
    
    * optimized imports
    
    * added tests for assignment 2

---
+++ src/test/java/Assignment2Test.java
@@ -0,0 +1,37 @@
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Assignment2Test {
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+
+    @Test
+    public void programCompiles() {
+        CDDriver.main(new String[]{""});
+    }
+
+    @Test
+    public void cdReader_reachesEndOfFile() {
+        CDReader reader = new CDReader();
+        reader.getAllCds("cds_short.txt");
+        assertEquals("Reached end of file\n",systemOutRule.getLogWithNormalizedLineSeparator());
+    }
+
+    @Test
+    public void cdReader_doesNotThrowException() {
+        CDReader reader = new CDReader();
+        assertDoesNotThrow((Executable) reader.getAllCds("cds_short.tx"));
+    }
+
+    @Test
+    public void cdReader_outputsMessageInsteadOfExiting() {
+        CDReader reader = new CDReader();
+        assertFalse(systemOutRule.getLog().isEmpty());
+    }
+}
