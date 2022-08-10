8542006904a0ed46f8c98701475069d37e35ef8b
Josh Taylor <joshmtaylor2000@gmail.com>
2022-08-10 11:54:36 -0500

    removed method so that readme makes more sense

--- src/main/java/Collections/CollectionManager.java
+++ src/main/java/Collections/CollectionManager.java
@@ -9,14 +9,10 @@ public class CollectionManager {
 
     public static void main(String[] args) {
         CdCollection cdCollection = getCdCollectionFromDriver(new CDDriver());
-        BookCollection bookCollection = getBookCollectionFromDriver(new BookDriver());
     }
 
     private static CdCollection getCdCollectionFromDriver(CDDriver cdDriver) {
         return cdDriver.getCdCollection("cds_short.txt");
     }
 
-    private static BookCollection getBookCollectionFromDriver(BookDriver bookDriver) {
-        return bookDriver.getBookCollection("gutenberg_books.csv");
-    }
 }
