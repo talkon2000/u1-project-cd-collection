432ad1108c7e8911c458bc131cb4afc3249dc5f5
Josh Taylor <107130895+talkon2000@users.noreply.github.com>
2022-08-10 15:20:52 -0500

    reformatted main branch to use csv files (#27)

--- src/main/java/Libraries/Book/BookReader.java
+++ src/main/java/Libraries/Book/BookReader.java
@@ -4,13 +4,12 @@ import Libraries.Book.Types.Book;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
-
 
 /**
  * This is a utility class with one method that should parse the data from an input file into a list of Books
@@ -23,10 +22,9 @@ public class BookReader {
      * @return a new list of every created Book object
      */
     public List<Book> getAllBooks(String filename) {
-
         List<Book> booksList = new ArrayList<>();
         try {
-            FileInputStream input = new FileInputStream(filename);
+            InputStream input = this.getClass().getClassLoader().getResourceAsStream(filename);
             Reader csvReader = new InputStreamReader(input);
 
             Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(csvReader);
@@ -37,6 +35,8 @@ public class BookReader {
                 }
                 booksList.add(new Book(Integer.parseInt(record.get(0)), record.get(1), record.get(2)));
             }
+            input.close();
+            System.out.println("Reached end of file");
         }
         catch (IOException | NullPointerException e) {
             System.out.println("An Exception occurred, so the file could not be read.");
--- src/main/java/Libraries/CD/CDReader.java
+++ src/main/java/Libraries/CD/CDReader.java
@@ -1,10 +1,14 @@
 package Libraries.CD;
 
 import Libraries.CD.Types.Cd;
+import Libraries.CD.Types.Song;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
-import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +16,7 @@ import java.util.List;
  * This is a utility class with one method that should parse the data from an input file into a list of Cds
  */
 public class CDReader {
+
     /**
      * This method should read every line in the file and create a number of Cd objects
      * @param filename: file to get data from
@@ -19,20 +24,35 @@ public class CDReader {
      */
     public List<Cd> getAllCds(String filename) {
         List<Cd> cdsList = new ArrayList<>();
+
         InputStream input = this.getClass().getClassLoader().getResourceAsStream(filename);
+        Reader csvReader = new InputStreamReader(input);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(csvReader);
+        for (CSVRecord record : records) {
+            //Ignores header line
+            if (record.getRecordNumber() == 1) {
+                continue;
+            }
+            String artist = record.get(0);
+            String title = record.get(1);
+            int year = Integer.parseInt(record.get(2));
+            int rating = Integer.parseInt(record.get(3));
+            int numOfSongs = Integer.parseInt(record.get(4));
+            Cd cd = new Cd(artist, title, year, rating, numOfSongs);
 
-        String line;
-        while ((line = reader.readLine()) != null) {
-            // TODO process the line
-            // Remember, the 5th line of each CD will tell you how many songs are part of that CD
-            // See the CDReader hint for more help.
-            // TODO create the CD and add to list
+            //This loop will get all the song data for you. You just have to make it into a song object
+            for (int i = 0; i < (record.size() - 5) / 2; i++) {
+                String title = record.get(i * 2 + 5);
+                String length = record.get(i * 2 + 6);
+                cd.addSong(/*TODO: implement*/);
+            }
+            cdsList.add(cd);
         }
+        input.close();
         System.out.println("Reached end of file");
-        reader.close();
 
         return cdsList;
     }
+
 }
